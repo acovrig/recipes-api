44c7caa2e2dab270106940695e8ba9d5c6d4acd7
Austin Covrig <accovrig@gmail.com>
2018-02-15 23:31:15 -0500

    Filled seeds like crazy

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,65 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-cat = Category.create(
-  name: %w(desert easy crockpot healthy).sample
-)
-recipe = Recipe.create(
-  name: Faker::Food.dish,
-  author: Faker::Name.name,
-  serving_size: Faker::Number.between(1,10),
-  serving_suggestion: Faker::Food.description,
-  rating: Faker::Number.decimal(1, 5)
-)
-cat.recipes << recipe
+(1..10).each do |ci|
+  print "Category #{ci} "
+  cat = Category.create(
+    name: %w(desert easy crockpot healthy).sample
+  )
+  print "\rCategory #{ci} #{cat.name} -> Recipe"
+  (1..10).each do |ri|
+    print "\rCategory #{ci} #{cat.name} -> Recipe #{ri}"
+    r = Recipe.create(
+      name: Faker::Food.dish,
+      author: Faker::Name.name,
+      serving_size: Faker::Number.between(1,10),
+      serving_suggestion: Faker::Food.measurement,
+      rating: Faker::Number.decimal(1, 5)
+    )
+    (1..10).each do |ui|
+      print "\rCategory #{ci} #{cat.name} -> Recipe #{ri} Utensil #{ui}                                      "
+      utensil = Utensil.create(
+        name: Faker::Food.spice,
+        qty: Faker::Number.between(1,5)
+      )
+      r.utensils << utensil
+    end
+    (1..10).each do |di|
+      print "\rCategory #{ci} #{cat.name} -> Recipe #{ri} Utensil Direction #{di}                             "
+      direction = Direction.create(
+        step: di,
+        action: Faker::Hacker.verb
+      )
+      r.directions << direction
+    end
+    (1..10).each do |ii|
+      print "\rCategory #{ci} #{cat.name} -> Recipe #{ri} Utensil Direction Ingredient #{ii}                   "
+      ingredient = Ingredient.create(
+        qty: Faker::Number.between(1, 10),
+        unit: Faker::Measurement.height("none"),
+        item: %w(can bag box).sample,
+        note: Faker::Lorem.sentence
+      )
+      r.ingredients << ingredient
+    end
+    (1..10).each do |ni|
+      print "\rCategory #{ci} #{cat.name} -> Recipe #{ri} Utensil Direction Ingredient Note #{ni}               "
+      note = Note.create(
+        note: Faker::Lorem.paragraph
+      )
+      r.notes << note
+    end
+    (1..10).each do |pi|
+      print "\rCategory #{ci} #{cat.name} -> Recipe #{ri} Utensil Direction Ingredient Note Picture #{pi}       "
+      picture = Picture.create(
+        fname: Faker::File.file_name,
+        sum: Faker::Internet.password(32, 32),
+        width: Faker::Number.between(800, 4000),
+        height: Faker::Number.between(800, 4000),
+        size: Faker::Number.between(800, 10000)
+      )
+      r.pictures << picture
+    end
+    cat.recipes << r
+  end
+end
