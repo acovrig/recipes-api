13b10d7e4a6bdfaa38941035b15aaae304ac4687
Austin Covrig <accovrig@gmail.com>
2018-03-24 19:37:13 -0400

    updated the category view a little

--- db/schema.rb
+++ db/schema.rb
@@ -12,42 +12,44 @@
 
 ActiveRecord::Schema.define(version: 20180216025541) do
 
-  create_table "categories", force: :cascade do |t|
+  create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "directions", force: :cascade do |t|
-    t.integer "recipe_id"
-    t.integer "step"
-    t.string "action"
+  create_table "directions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.bigint "recipe_id", null: false
+    t.integer "step", null: false
+    t.string "action", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["recipe_id", "step"], name: "index_directions_on_recipe_id_and_step", unique: true
     t.index ["recipe_id"], name: "index_directions_on_recipe_id"
   end
 
-  create_table "ingredients", force: :cascade do |t|
-    t.integer "recipe_id"
-    t.float "qty"
+  create_table "ingredients", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.bigint "recipe_id", null: false
+    t.float "qty", limit: 24
     t.string "unit"
-    t.string "item"
+    t.string "item", null: false
     t.text "note"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["recipe_id", "item"], name: "index_ingredients_on_recipe_id_and_item", unique: true
     t.index ["recipe_id"], name: "index_ingredients_on_recipe_id"
   end
 
-  create_table "notes", force: :cascade do |t|
-    t.integer "recipe_id"
+  create_table "notes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.bigint "recipe_id"
     t.text "note"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["recipe_id"], name: "index_notes_on_recipe_id"
   end
 
-  create_table "pictures", force: :cascade do |t|
-    t.integer "recipe_id"
+  create_table "pictures", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.bigint "recipe_id"
     t.string "fname"
     t.string "sum"
     t.integer "width"
@@ -58,32 +60,40 @@ ActiveRecord::Schema.define(version: 20180216025541) do
     t.index ["recipe_id"], name: "index_pictures_on_recipe_id"
   end
 
-  create_table "recipe_categories", force: :cascade do |t|
-    t.integer "recipe_id"
-    t.integer "category_id"
+  create_table "recipe_categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.bigint "recipe_id"
+    t.bigint "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_recipe_categories_on_category_id"
     t.index ["recipe_id"], name: "index_recipe_categories_on_recipe_id"
   end
 
-  create_table "recipes", force: :cascade do |t|
+  create_table "recipes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
     t.string "author"
     t.integer "serving_size"
     t.string "serving_suggestion"
-    t.float "rating"
+    t.float "rating", limit: 24
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "utensils", force: :cascade do |t|
-    t.integer "recipe_id"
+  create_table "utensils", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.bigint "recipe_id"
     t.string "name"
     t.integer "qty"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["recipe_id", "name"], name: "uq_utensil", unique: true
     t.index ["recipe_id"], name: "index_utensils_on_recipe_id"
   end
 
+  add_foreign_key "directions", "recipes"
+  add_foreign_key "ingredients", "recipes"
+  add_foreign_key "notes", "recipes"
+  add_foreign_key "pictures", "recipes"
+  add_foreign_key "recipe_categories", "categories"
+  add_foreign_key "recipe_categories", "recipes"
+  add_foreign_key "utensils", "recipes"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,70 +6,163 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-cats = %w(desert easy crockpot healthy)
-cats.each do |cat|
-  print "\rCategory #{cat} -> Recipe"
-  cat = Category.create(
-    name: cat
+crockpot = Category.create(name: 'crockpot')
+potato_soup = Recipe.create(
+  name: 'Potato Soup',
+  author: 'Austin Covrig'
+)
+potato_soup.ingredients << [
+  Ingredient.create(
+    qty: 1,
+    unit: 'pkg',
+    item: 'Ore Ida Potatoes Obrien'
+  ),
+  Ingredient.create(
+    qty: 1,
+    unit: 'cup',
+    item: 'water'
+  ),
+  Ingredient.create(
+    qty: 1,
+    unit: 'can',
+    item: 'cream of celery soup'
+  ),
+  Ingredient.create(
+    qty: 4,
+    unit: 'oz',
+    item: 'cream cheese'
+  ),
+  Ingredient.create(
+    qty: 0.5,
+    unit: 'cup',
+    item: 'milk'
   )
-  (1..10).each do |ri|
-    print "\rCategory #{cat.name} -> Recipe #{ri}"
-    r = Recipe.create(
-      name: Faker::Food.dish,
-      author: Faker::Name.name,
-      serving_size: Faker::Number.between(1,10),
-      serving_suggestion: Faker::Food.measurement,
-      rating: Faker::Number.decimal(1, 5)
-    )
-    (1..10).each do |ui|
-      print "\rCategory #{cat.name} -> Recipe #{ri} Utensil #{ui}                                      "
-      utensil = Utensil.create(
-        recipe: r,
-        name: Faker::Food.spice,
-        qty: Faker::Number.between(1,5)
-      )
-    end
-    (1..10).each do |di|
-      print "\rCategory #{cat.name} -> Recipe #{ri} Utensil Direction #{di}                             "
-      direction = Direction.create(
-        step: di,
-        action: Faker::Hacker.verb
-      )
-      r.directions << direction
-    end
-    (1..10).each do |ii|
-      print "\rCategory #{cat.name} -> Recipe #{ri} Utensil Direction Ingredient #{ii}                   "
-      ingredient = Ingredient.create(
-        qty: Faker::Number.between(1, 10),
-        unit: Faker::Measurement.height("none"),
-        item: Faker::Food.spice,
-        note: Faker::Lorem.sentence
-      )
-      r.ingredients << ingredient
-    end
-    (1..10).each do |ni|
-      print "\rCategory #{cat.name} -> Recipe #{ri} Utensil Direction Ingredient Note #{ni}               "
-      note = Note.create(
-        note: Faker::Lorem.paragraph
-      )
-      r.notes << note
-    end
-    (1..10).each do |pi|
-      print "\rCategory #{cat.name} -> Recipe #{ri} Utensil Direction Ingredient Note Picture #{pi}       "
-      picture = Picture.create(
-        fname: Faker::File.file_name,
-        sum: Faker::Internet.password(32, 32),
-        width: Faker::Number.between(800, 4000),
-        height: Faker::Number.between(800, 4000),
-        size: Faker::Number.between(800, 10000)
-      )
-      r.pictures << picture
-    end
-    begin
-      cat.recipes << r
-    rescue
-      next
-    end
-  end
-  puts ''
-end
+]
+potato_soup.directions << [
+  Direction.create(
+    step: 1,
+    action: 'Put hte potatoes, water, and soup in the crockpot on medium for 6 hours.'
+  ),
+  Direction.create(
+    step: 2,
+    action: 'Stir in the cheese and milk before serving.'
+  )
+]
+potato_soup.utensils << [
+  Utensil.create(name: 'Crockpot', qty: 1),
+  Utensil.create(name: 'Stirring Spoon', qty: 1)
+]
+
+spice_cake = Recipe.create(
+  name: 'Apple Spice Cake',
+  author: 'Austin Covrig'
+)
+spice_cake.ingredients << [
+  Ingredient.create(
+    qty: 2,
+    unit: 'cans',
+    item: 'apple pie filling',
+    note: '20oz cans'
+  ),
+  Ingredient.create(
+    qty: 1,
+    unit: 'box',
+    item: 'spice cake mix'
+  ),
+  Ingredient.create(
+    qty: 8,
+    unit: 'oz',
+    item: 'butter'
+  )
+]
+spice_cake.directions << [
+  Direction.create(
+    step: 1,
+    action: 'Pour apples into bottom of crockpot.'
+  ),
+  Direction.create(
+    step: 2,
+    action: 'Spread cake mix over apples.'
+  ),
+  Direction.create(
+    step: 3,
+    action: 'Place butter ontop of cake mix.'
+  ),
+  Direction.create(
+    step: 4,
+    action: '3 hours on high.'
+  )
+]
+spice_cake.utensils << [
+  Utensil.create(name: 'Crockpot', qty: 1)
+]
+
+crockpot.recipes << [potato_soup, spice_cake]
+
+# cats = %w(desert easy crockpot healthy)
+# cats.each do |cat|
+#   print "\rCategory #{cat} -> Recipe"
+#   cat = Category.create(
+#     name: cat
+#   )
+#   (1..10).each do |ri|
+#     print "\rCategory #{cat.name} -> Recipe #{ri}"
+#     r = Recipe.create(
+#       name: Faker::Food.dish,
+#       author: Faker::Name.name,
+#       serving_size: Faker::Number.between(1,10),
+#       serving_suggestion: Faker::Food.measurement,
+#       rating: Faker::Number.decimal(1, 5)
+#     )
+#     (1..10).each do |ui|
+#       print "\rCategory #{cat.name} -> Recipe #{ri} Utensil #{ui}                                      "
+#       utensil = Utensil.create(
+#         recipe: r,
+#         name: Faker::Food.spice,
+#         qty: Faker::Number.between(1,5)
+#       )
+#     end
+#     (1..10).each do |di|
+#       print "\rCategory #{cat.name} -> Recipe #{ri} Utensil Direction #{di}                             "
+#       direction = Direction.create(
+#         step: di,
+#         action: Faker::Hacker.verb
+#       )
+#       r.directions << direction
+#     end
+#     (1..10).each do |ii|
+#       print "\rCategory #{cat.name} -> Recipe #{ri} Utensil Direction Ingredient #{ii}                   "
+#       ingredient = Ingredient.create(
+#         qty: Faker::Number.between(1, 10),
+#         unit: Faker::Measurement.height("none"),
+#         item: Faker::Food.spice,
+#         note: Faker::Lorem.sentence
+#       )
+#       r.ingredients << ingredient
+#     end
+#     (1..10).each do |ni|
+#       print "\rCategory #{cat.name} -> Recipe #{ri} Utensil Direction Ingredient Note #{ni}               "
+#       note = Note.create(
+#         note: Faker::Lorem.paragraph
+#       )
+#       r.notes << note
+#     end
+#     (1..10).each do |pi|
+#       print "\rCategory #{cat.name} -> Recipe #{ri} Utensil Direction Ingredient Note Picture #{pi}       "
+#       picture = Picture.create(
+#         fname: Faker::File.file_name,
+#         sum: Faker::Internet.password(32, 32),
+#         width: Faker::Number.between(800, 4000),
+#         height: Faker::Number.between(800, 4000),
+#         size: Faker::Number.between(800, 10000)
+#       )
+#       r.pictures << picture
+#     end
+#     begin
+#       cat.recipes << r
+#     rescue
+#       next
+#     end
+#   end
+#   puts ''
+# end
