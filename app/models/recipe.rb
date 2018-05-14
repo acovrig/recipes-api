e66bdedf7392e2c5ec38d87745eab9ac8f885297
Austin Covrig <accovrig@gmail.com>
2018-05-13 23:09:47 -0400

    Updated utensil model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,20 +1,28 @@
 class Recipe < ApplicationRecord
-    belongs_to :author, class_name: 'User'
-    has_many :recipe_categories
-    has_many :categories, through: :recipe_categories
-    has_many :directions
-    has_many :ingredients
-    has_many :utensils
-    has_many :pictures
-    has_many :notes
+  before_save :normalize_blank_values
+  belongs_to :author, class_name: 'User'
+  has_many :recipe_categories
+  has_many :categories, through: :recipe_categories
+  has_many :directions
+  has_many :ingredients
+  has_many :utensils
+  has_many :pictures
+  has_many :notes
 
-    validates :name, :privacy, presence: true
-    validates :name, uniqueness: true
+  validates :name, :privacy, presence: true
+  validates :name, uniqueness: true
 
-    accepts_nested_attributes_for :directions, :ingredients, :utensils, reject_if: :all_blank, allow_destroy: true
+  accepts_nested_attributes_for :directions, :ingredients, :utensils, reject_if: :all_blank, allow_destroy: true
 
-    scope :public_recipes, -> { where(privacy: 'public') }
-    scope :internal_recipes, -> { where(privacy: 'internal') }
-    scope :unlisted_recipes, -> { where(privacy: 'unlisted') }
-    scope :private_recipes, -> { where(privacy: 'private') }
+  scope :public_recipes, -> { where(privacy: 'public') }
+  scope :internal_recipes, -> { where(privacy: 'internal') }
+  scope :unlisted_recipes, -> { where(privacy: 'unlisted') }
+  scope :private_recipes, -> { where(privacy: 'private') }
+
+  private
+    def normalize_blank_values
+      attributes.each do |col, val|
+        self[col].present? || self[col] = nil
+      end
+    end
 end
--- app/models/utensil.rb
+++ app/models/utensil.rb
@@ -1,5 +1,5 @@
 class Utensil < ApplicationRecord
   belongs_to :recipe
-  validates :name, presence: true
+  validates :name, :qty, presence: true
   validates :name, uniqueness: {scope: [:recipe_id]}
 end
